d964e844044278bf0faf8e756ae61c30dc896c7a
Jens Axboe <axboe@kernel.dk>
2024-01-28 20:08:24 -0700

    io_uring: add io_file_can_poll() helper
    This adds a flag to avoid dipping dereferencing file and then f_op
    to figure out if the file has a poll handler defined or not. We
    generally call this at least twice for networked workloads.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/io_uring_types.h
+++ include/linux/io_uring_types.h
@@ -463,6 +463,7 @@ enum {
 	REQ_F_SUPPORT_NOWAIT_BIT,
 	REQ_F_ISREG_BIT,
 	REQ_F_POLL_NO_LAZY_BIT,
+	REQ_F_CAN_POLL_BIT,
 
 	/* not a real bit, just to check we're not overflowing the space */
 	__REQ_F_LAST_BIT,
@@ -532,6 +533,8 @@ enum {
 	REQ_F_HASH_LOCKED	= BIT(REQ_F_HASH_LOCKED_BIT),
 	/* don't use lazy poll wake for this request */
 	REQ_F_POLL_NO_LAZY	= BIT(REQ_F_POLL_NO_LAZY_BIT),
+	/* file is pollable */
+	REQ_F_CAN_POLL		= BIT(REQ_F_CAN_POLL_BIT),
 };
 
 typedef void (*io_req_tw_func_t)(struct io_kiocb *req, struct io_tw_state *ts);
