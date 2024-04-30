d5bdd53200a05a341e122414d71a0cf3cb9104dd
Kent Overstreet <kent.overstreet@linux.dev>
2024-04-30 15:37:25 -0400

    bcachefs: bch2_dev_iterate()
    New helper for getting refs to devices as we iterate.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb-members.h
+++ fs/bcachefs/sb-members.h
@@ -241,6 +241,15 @@ static inline struct bch_dev *bch2_dev_bucket_tryget(struct bch_fs *c, struct bp
 	return ca;
 }
 
+static inline struct bch_dev *bch2_dev_iterate(struct bch_fs *c, struct bch_dev *ca, unsigned dev_idx)
+{
+	if (ca && ca->dev_idx == dev_idx)
+		return ca;
+	if (ca)
+		percpu_ref_put(&ca->ref);
+	return bch2_dev_tryget(c, dev_idx);
+}
+
 /* XXX kill, move to struct bch_fs */
 static inline struct bch_devs_mask bch2_online_devs(struct bch_fs *c)
 {
