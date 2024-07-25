75a4b53cbc9dfc38cfa32907a68987e1c2149db1
Charlie Jenkins <charlie@rivosinc.com>
2024-07-24 21:16:27 -0700

    riscv: hwprobe: Add thead vendor extension probing
    Add a new hwprobe key "RISCV_HWPROBE_KEY_VENDOR_EXT_THEAD_0" which
    allows userspace to probe for the new RISCV_ISA_VENDOR_EXT_XTHEADVECTOR
    vendor extension.
    
    This new key will allow userspace code to probe for which thead vendor
    extensions are supported. This API is modeled to be consistent with
    RISCV_HWPROBE_KEY_IMA_EXT_0. The bitmask returned will have each bit
    corresponding to a supported thead vendor extension of the cpumask set.
    Just like RISCV_HWPROBE_KEY_IMA_EXT_0, this allows a userspace program
    to determine all of the supported thead vendor extensions in one call.
    
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>
    Reviewed-by: Evan Green <evan@rivosinc.com>
    Link: https://lore.kernel.org/r/20240724-xtheadvector-v8-10-cf043168e137@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/include/asm/hwprobe.h
+++ arch/riscv/include/asm/hwprobe.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- * Copyright 2023 Rivos, Inc
+ * Copyright 2023-2024 Rivos, Inc
  */
 
 #ifndef _ASM_HWPROBE_H
@@ -21,6 +21,7 @@ static inline bool hwprobe_key_is_bitmask(__s64 key)
 	case RISCV_HWPROBE_KEY_BASE_BEHAVIOR:
 	case RISCV_HWPROBE_KEY_IMA_EXT_0:
 	case RISCV_HWPROBE_KEY_CPUPERF_0:
+	case RISCV_HWPROBE_KEY_VENDOR_EXT_THEAD_0:
 		return true;
 	}
 
